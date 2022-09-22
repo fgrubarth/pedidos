e53541fb9904108759006b51f2ada365e12098ac
fgrubarth <fgrubart@gmail.com>
2022-09-22 10:25:15 -0300

    puse identificador en html y boton eliminar en js

--- script.js
+++ script.js
@@ -20,27 +20,72 @@ const magia = document.querySelector(
 
   function agregarCart(itemTitulo, itemPrecio, itemImagen){
     const div = document.createElement('div');
-  const pedido = `<div class="row ">
+  const pedido = `<div class="row itemCompra">
   <div class="col-6">
       <div class=" d-flex align-items-center h-100 border-bottom pb-2 pt-3">
           <img src=${itemImagen} class="imagenCarro">
-          <h6 class=" text-truncate ml-3 mb-0">${itemTitulo}</h6>
+          <h6 class="itemCompraTitulo text-truncate ml-3 mb-0">${itemTitulo}</h6>
       </div>
   </div>
   <div class="col-2">
       <div class=" d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-          <p class=" mb-0 ">${itemPrecio}</p>
+          <p class="itemCompraPrecio mb-0 ">${itemPrecio}</p>
       </div>
   </div>
   <div class="col-4">
       <div
           class=" d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
-          <input type="number"
+          <input class="itemCompraCantidad" type="number"
               value="1" min="1">
-          <button class="btn btn-danger" type="button">X</button>
+          <button class="btn btn-danger borrar" type="button">X</button>
       </div>
   </div>
 </div>`
 div.innerHTML = pedido;
-  magia.append(div);
-  }
+magia.append(div);
+
+div.querySelector(".borrar").addEventListener("click", borrar)
+
+aumentarPrecio();
+}
+
+function aumentarPrecio(){
+  let total= 0;
+  const precioTotal = document.querySelector('.precioTotales');
+  
+  const itemCompras = document.querySelectorAll('.itemCompra')
+  
+  
+  itemCompras.forEach((itemCompra) => {
+    const itemPrecioTotal = itemCompra.querySelector(
+      '.itemCompraPrecio'
+      
+    );
+   
+  const precio = itemPrecioTotal.textContent.replace("$", "");
+  console.log(precio)
+  
+  const cantidadTotal = itemCompra.querySelector(
+    '.itemCompraCantidad'
+  );
+  
+  const itemCompraCantidad = 
+    cantidadTotal.value;
+    console.log(itemCompraCantidad)
+  total = total + precio * itemCompraCantidad;
+  console.log(total)
+  });
+  precioTotal.innerHTML = `$${total.toFixed(0)}`;
+
+}
+
+function borrar(event){
+  const botonBorrar = event.target
+  botonBorrar.closest('.itemCompra').remove();
+  aumentarPrecio();
+
+}
+
+
+
+
