a417b373fa8c152b77db399af93ed56fe18d0bc8
fgrubarth <fgrubart@gmail.com>
2022-09-28 19:40:41 -0300

    empece

--- script.js
+++ script.js
@@ -32,129 +32,159 @@
 // console.log(agregar)
 
 
-const agregar = document.querySelectorAll('#agregar');
-agregar.forEach((agregarPedido) => {
-  agregarPedido.addEventListener('click', hacerClick);
-});
 
-const magia = document.querySelector(
-    '.magia'
-  );
+
+class carrito{
+  //para añadir al carrito
+      comprarProducto(e){
+        e.preventDefault();
+        //Delegado para agregar al carrito
+        if(e.target.classList.contains('agregar-carrito')){
+            const producto = e.target.parentElement.parentElement.parentElement;
+            //Enviamos el producto seleccionado para tomar sus datos
+            // this.leerDatosProducto(producto);
+            console.log(producto)
+        }
+}
+}
+const carro = new carrito()
+const magia = document.querySelector('.magia');
+const productos = document.getElementsByClassName("pedir")
+
+
+cargarEventos();
+
+function cargarEventos(){
+
+    //Se ejecuta cuando se presionar agregar carrito
+    productos.
+  }
+
+
+
+// const agregar = document.querySelectorAll('#agregar');
+// agregar.forEach((agregarPedido) => {
+//   agregarPedido.addEventListener('click', hacerClick);
+// });
+
+// const magia = document.querySelector(
+//     '.magia'
+//   );
   
-  function hacerClick(event) {
-    const boton = event.target;
-    const item = boton.closest('.contenedor');
+//   function hacerClick(event) {
+//     const boton = event.target;
+//     const item = boton.closest('.contenedor');
   
-    const itemTitulo = item.querySelector('.titulo').textContent;
-    const itemPrecio = item.querySelector('.precio').textContent;
-    const itemImagen = item.querySelector('.foto').src;
+//     const itemTitulo = item.querySelector('.titulo').textContent;
+//     const itemPrecio = item.querySelector('.precio').textContent;
+//     const itemImagen = item.querySelector('.foto').src;
     
-    agregarCart(itemTitulo, itemPrecio, itemImagen);
-  }
+//     agregarCart(itemTitulo, itemPrecio, itemImagen);
+//   }
 
-  function agregarCart(itemTitulo, itemPrecio, itemImagen){
-    const elementsTitle = magia.getElementsByClassName(
-      'itemCompraTitulo'
-    );
-    for (let i = 0; i < elementsTitle.length; i++) {
-      if (elementsTitle[i].innerText === itemTitulo) {
-        let elementQuantity = elementsTitle[
-          i
-        ].parentElement.parentElement.parentElement.querySelector(
-          '.itemCompraCantidad'
-        );
-        elementQuantity.value++;
-        aumentarPrecio();
-        return;
-      }
-    }
+//   function agregarCart(itemTitulo, itemPrecio, itemImagen){
+//     const elementsTitle = magia.getElementsByClassName(
+//       'itemCompraTitulo'
+//     );
+//     for (let i = 0; i < elementsTitle.length; i++) {
+//       if (elementsTitle[i].innerText === itemTitulo) {
+//         let elementQuantity = elementsTitle[
+//           i
+//         ].parentElement.parentElement.parentElement.querySelector(
+//           '.itemCompraCantidad'
+//         );
+//         elementQuantity.value++;
+//         aumentarPrecio();
+//         return;
+//       }
+//     }
 
 
-  const div = document.createElement('div');
-  const pedido = `<div class="row itemCompra">
-  <div class="col-6">
-      <div class=" d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-          <img src=${itemImagen} class="imagenCarro">
-          <h6 class="itemCompraTitulo text-truncate ml-3 mb-0">${itemTitulo}</h6>
-      </div>
-  </div>
-  <div class="col-2">
-      <div class=" d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-          <p class="itemCompraPrecio mb-0 ">${itemPrecio}</p>
-      </div>
-  </div>
-  <div class="col-4">
-      <div
-          class=" d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
-          <input class="itemCompraCantidad" type="number"
-              value="1">
-          <button class="btn btn-danger borrar" type="button">X</button>
-      </div>
-  </div>
-</div>`
-div.innerHTML = pedido;
-magia.append(div);
+//   const div = document.createElement('div');
+//   const pedido = `<div class="row itemCompra">
+//   <div class="col-6">
+//       <div class=" d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+//           <img src=${itemImagen} class="imagenCarro">
+//           <h6 class="itemCompraTitulo text-truncate ml-3 mb-0">${itemTitulo}</h6>
+//       </div>
+//   </div>
+//   <div class="col-2">
+//       <div class=" d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+//           <p class="itemCompraPrecio mb-0 ">${itemPrecio}</p>
+//       </div>
+//   </div>
+//   <div class="col-4">
+//       <div
+//           class=" d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
+//           <input class="itemCompraCantidad" type="number"
+//               value="1">
+//           <button class="btn btn-danger borrar" type="button">X</button>
+//       </div>
+//   </div>
+// </div>`
+// div.innerHTML = pedido;
+// magia.append(div);
 
-div.querySelector(".borrar").addEventListener("click", borrar)
+// div.querySelector(".borrar").addEventListener("click", borrar)
 
-div.querySelector('.itemCompraCantidad').addEventListener('change', cambiarCantidad);
-// actualizarProductosStorage()
-// obtenerProductosStorage()
-aumentarPrecio();
-}
+// div.querySelector('.itemCompraCantidad').addEventListener('change', cambiarCantidad);
+// // actualizarProductosStorage()
+// // obtenerProductosStorage()
+// aumentarPrecio();
+// }
 
-function aumentarPrecio(){
-  let total= 0;
-  const precioTotal = document.querySelector('.precioTotales');
+// function aumentarPrecio(){
+//   let total= 0;
+//   const precioTotal = document.querySelector('.precioTotales');
   
-  const itemCompras = document.querySelectorAll('.itemCompra')
+//   const itemCompras = document.querySelectorAll('.itemCompra')
   
   
-  itemCompras.forEach((itemCompra) => {
-    const itemPrecioTotal = itemCompra.querySelector(
-      '.itemCompraPrecio'
+//   itemCompras.forEach((itemCompra) => {
+//     const itemPrecioTotal = itemCompra.querySelector(
+//       '.itemCompraPrecio'
       
-    );
+//     );
    
-  const precio = itemPrecioTotal.textContent.replace("$", "");
-  console.log(precio)
+//   const precio = itemPrecioTotal.textContent.replace("$", "");
+//   console.log(precio)
   
-  const cantidadTotal = itemCompra.querySelector(
-    '.itemCompraCantidad'
-  );
+//   const cantidadTotal = itemCompra.querySelector(
+//     '.itemCompraCantidad'
+//   );
   
-  const itemCompraCantidad = 
-    cantidadTotal.value;
-    console.log(itemCompraCantidad)
-  total = total + precio * itemCompraCantidad;
-  console.log(total)
-  });
-  precioTotal.innerHTML = `$${total.toFixed(0)}`;
+//   const itemCompraCantidad = 
+//     cantidadTotal.value;
+//     console.log(itemCompraCantidad)
+//   total = total + precio * itemCompraCantidad;
+//   console.log(total)
+//   });
+//   precioTotal.innerHTML = `$${total.toFixed(0)}`;
 
-}
+// }
 
-function cambiarCantidad(event) {
-  const input = event.target;
-  input.value <= 0 ? (input.value = 1) : null;
-  aumentarPrecio();
-}
+// function cambiarCantidad(event) {
+//   const input = event.target;
+//   input.value <= 0 ? (input.value = 1) : null;
+//   aumentarPrecio();
+// }
 
-function borrar(event){
-  const botonBorrar = event.target
-  botonBorrar.closest('.itemCompra').remove();
-  aumentarPrecio();
+// function borrar(event){
+//   const botonBorrar = event.target
+//   botonBorrar.closest('.itemCompra').remove();
+//   aumentarPrecio();
 
-}
+// }
 
-function actualizarProductosStorage() {
-  let productosJSON = JSON.stringify(agregar);
-  localStorage.setItem("agregar", productosJSON);
-}
+// function actualizarProductosStorage() {
+//   let productosJSON = JSON.stringify(agregar);
+//   localStorage.setItem("agregar", productosJSON);
+// }
 
-function obtenerProductosStorage() {
-  let productosJSON = localStorage.getItem("agregar");
-  if (productosJSON) {
-    agregar = JSON.parse(productosJSON);
-    agregarCart();
-  }
-}
+// function obtenerProductosStorage() {
+//   let productosJSON = localStorage.getItem("agregar");
+//   if (productosJSON) {
+//     agregar = JSON.parse(productosJSON);
+//     agregarCart();
+//   }
+// }
